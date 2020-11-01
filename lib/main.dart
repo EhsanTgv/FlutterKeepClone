8a7f53291e7bf909559b1191b92b7a5603c52738
Ehsan Taghavi <ehsantgv@gmail.com>
2020-11-01 23:44:13 +0330

    Fix deprecated items

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,19 @@ void main() => runApp(NotesApp());
 class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => StreamProvider.value(
-    initialData: CurrentUser.initial,
-    value: FirebaseAuth.instance.onAuthStateChanged.map((user) => CurrentUser.create(user)),
-    child: Consumer<CurrentUser>(
-      builder: (context, user, _) => MaterialApp(
-        title: 'Flutter Keep',
-        home: user.isInitialValue
-          ? Scaffold(body: const Text('Loading...'))
-          : user.data != null ? HomeScreen() : LoginScreen(),
-      ),
-    ),
-  );
+        initialData: CurrentUser.initial,
+        value: FirebaseAuth.instance
+            .authStateChanges()
+            .map((user) => CurrentUser.create(user)),
+        child: Consumer<CurrentUser>(
+          builder: (context, user, _) => MaterialApp(
+            title: 'Flutter Keep',
+            home: user.isInitialValue
+                ? Scaffold(body: const Text('Loading...'))
+                : user.data != null
+                    ? HomeScreen()
+                    : LoginScreen(),
+          ),
+        ),
+      );
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,10 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class CurrentUser {
   final bool isInitialValue;
-  final FirebaseUser data;
+  final User data;
 
   const CurrentUser._(this.data, this.isInitialValue);
-  factory CurrentUser.create(FirebaseUser data) => CurrentUser._(data, false);
+  factory CurrentUser.create(User data) => CurrentUser._(data, false);
 
   /// The inital empty instance.
   static const initial = CurrentUser._(null, true);
