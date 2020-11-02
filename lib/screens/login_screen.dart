a4f51710db5c24b99bed01e5c5a111c63fd495ca
Ehsan Taghavi <ehsantgv@gmail.com>
2020-11-02 22:55:35 +0330

    Implement Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,13 +1,63 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  State<StatefulWidget> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _auth = FirebaseAuth.instance;
+  final _googleSignIn = GoogleSignIn();
+  String _errorMessage;
+  bool _loggingIn = false;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: _signInWithGoogle,
+                child: const Text('Continue with Google'),
+              ),
+              if (_errorMessage != null)
+                Text(
+                  _errorMessage,
+                  style: const TextStyle(color: Colors.red),
+                ),
+            ],
+          ),
+        ),
+      );
+
+  void _signInWithGoogle() async {
+    _setLoggingIn(); // show progress
+    String errMsg;
+
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      final googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.getCredential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken,
+      );
+      await _auth.signInWithCredential(credential);
+    } catch (e) {
+      errMsg = 'Login failed, please try again later.';
+    } finally {
+      _setLoggingIn(false, errMsg); // always stop the progress indicator
+    }
+  }
+
+  /// update the logging-in indicator, & show error message if any
+  void _setLoggingIn([bool loggingIn = true, String errMsg]) {
+    if (mounted) {
+      setState(() {
+        _loggingIn = loggingIn;
+        _errorMessage = errMsg;
+      });
+    }
   }
 }
